4842b6868653977bb019b6795e240a34673af6b5
AzAbid1 <73388494+AzAbid1@users.noreply.github.com>
2022-10-16 00:27:33 +0100

    answer done + dynamic buttons done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'question.dart';
+import 'answer.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,21 +15,31 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
+  int _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
-    print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext ctx) {
     var questions = [
-      'What\' your favourite color?',
-      'What\'s your favourite animal?'
+      {
+        'questionText': 'What\' your favourite color?',
+        'answers': ['Black', 'Red', 'Green', 'White']
+      },
+      {
+        'questionText': 'What\'s your favourite animal?',
+        'answers': ['Dog', 'Rabbit', 'Horse', 'Wolf']
+      },
+      {
+        'questionText': 'Who\'s your favourite instructor?',
+        'answers': ['Dan', 'Rob', 'Hanna', 'Walter']
+      }
     ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -36,16 +47,10 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Question(questions[_questionIndex]),
-            ElevatedButton(onPressed: _answerQuestion, child: Text('Answer 1')),
-            ElevatedButton(
-                onPressed: () => print('Answer 2 chosen!'),
-                child: Text('Answer 2')),
-            ElevatedButton(
-                onPressed: () {
-                  print('Answer 3 chosen!');
-                },
-                child: Text('Answer 3')),
+            Question(questions[_questionIndex]['questionText'] as String),
+        ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),
