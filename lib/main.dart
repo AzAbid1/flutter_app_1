66123e407f5911a0461e6f039721e5743ff9d107
AzAbid1 <73388494+AzAbid1@users.noreply.github.com>
2022-10-16 23:45:54 +0100

    added reset

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,14 @@ class _MyAppState extends State<MyApp> {
   int _questionIndex = 0;
   var _totalScore = 0;
 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+
+  }
+
   void _answerQuestion(int score) {
     _totalScore += score;
 
@@ -68,7 +76,7 @@ class _MyAppState extends State<MyApp> {
                 answerQuestion: _answerQuestion,
                 questionIndex: _questionIndex,
                 questions: _questions)
-            : Result(_totalScore),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
